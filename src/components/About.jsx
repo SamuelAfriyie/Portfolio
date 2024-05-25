527e0ed9203908417c8df2af2a52f2d0099dfaa6
SamuelAfriyie <judesmith01234@gmail.com>
2024-05-25 21:44:22 +0000

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        A versatile software developer proficient in front-end, back-end, and
+        mobile app development. Skilled in creating responsive and user-friendly
+        interfaces using modern web technologies such as HTML, CSS, and
+        JavaScript. Experienced in building robust server-side applications with
+        languages like Node.js, Python, or Java, and familiar with databases
+        like MySQL, MongoDB, or PostgreSQL. Additionally, adept at developing
+        cross-platform mobile applications using frameworks such as React Native
+        or Flutter. Passionate about creating seamless digital experiences and
+        continuously learning new technologies to stay at the forefront of the
+        industry.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, "about");
